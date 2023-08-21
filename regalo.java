2ba7028af0e6fa03075785df1849bc5720bbe15a
Josemt52 <129121487+Josemt52@users.noreply.github.com>
2023-08-20 23:18:27 -0500

    Update and rename archivo..txt to regalo.java

---
+++ regalo.java
@@ -0,0 +1,11 @@
+public class regalo {
+    public static String elegir (int vida){
+        String gift = "";
+        switch(vida){
+        case 1: gift="Un pasaje al caribe";break;
+        case 2: gift="Un pasaje al caribe";break;
+        case 3: gift="Un pasaje al caribe";break;
+     }
+     return gift;
+}
+}
